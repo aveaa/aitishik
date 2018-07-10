0543b9e32b27dca8848e962b8db8e7872d054137
Andrey0189 <39239738+Andrey0189@users.noreply.github.com>
2018-07-10 20:26:27 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ bot.on('message', message => {
     if (['ship', 'love', 'шип'].includes(command)) {
         if (!args[0]) args[0] = message.author
         if (!args[1]) args[1] = message.guild.members.random();
-        let percents = randomInteger(0, 20) * 5;
+        let percents = randomInteger(0, 20) * 5 + '%';
         message.channel.send(percents)
         //message.channel.send('**Проверка на любовь...**\n`' + args[0] + '▼`\n`' + args[1] + '▲`\n\n');
     }
