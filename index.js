c15275a1ae938bcd744c232ef0fefc404f5c659a
Andrey0189 <39239738+Andrey0189@users.noreply.github.com>
2018-07-10 21:16:19 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ bot.on('message', message => {
         const embed = new Discord.RichEmbed()
                 .setTitle("МАТЧМЕЙКИНГ")
                 .setColor("af00ff")
-                .setDescription('▼***' + args[0] + '*** `▲`\n***' + args[1] + '***\n\n' + percents + '% `[' + blackShkala + whiteShkala + ']`\n\nВердикт: **undefind** :D')
+                .setDescription('▼***' + args[0] + '***\n▲***' + args[1] + '***\n\n' + percents + '% `[' + blackShkala + whiteShkala + ']`\n\nВердикт: **undefind** :D')
                 .setFooter(bot_name + " | " + version + " | Все права защищены")
                 .setTimestamp();
             message.channel.send({embed});
