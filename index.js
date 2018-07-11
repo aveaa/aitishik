4a2853a904cc09d90a56c480a6e333cb3c274730
Andrey0189 <39239738+Andrey0189@users.noreply.github.com>
2018-07-11 20:19:59 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -208,7 +208,8 @@ bot.on('message', message => {
         args[1].split('').forEach( (letter) => {
             args1percents += letterCheckng(letter);
         })
-        percents = args0percents + args1percents
+        percents = args0percents + args1percents;
+        if (isNaN(percents)) percents = randomInteger(0, 100);
         if (percents <= 99) {loveText = 'Невероятно!!!:heart_eyes:'; shkala = '■■■■■■■■■□';}
         if (percents <= 89) {loveText = 'Превосходно!:heartpulse:'; shkala = '■■■■■■■■□□';}
         if (percents <= 79) {loveText = 'Ууу ( ͡° ͜ʖ ͡°)'; shkala = '■■■■■■■□□□';}
